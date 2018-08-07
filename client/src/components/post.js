1267c3cae1206bf02e63fe1f7cf6ca839d2bf44a
Andrius Palivonas <andrius@palivonas.lt>
2018-08-07 11:53:25 +0300

    Use markdown for posts Show published date and author ID

--- client/src/components/post.js
+++ client/src/components/post.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import marked from 'marked';
+import 'github-markdown-css';
 
 import './post.css';
+
+const getHtml = (md) => {
+	const __html = marked(md, { sanitize: true, breaks: true });
+	return { __html };
+};
 
 const Post = ({ post }) => (
 	<article className="post" key={post.id}>
 		<h2><Link to={`/posts/${post.id}`}>{ post.title }</Link></h2>
-		<div className="content-text">
-			<p>{ post.content }</p>
-		</div>
+		<small>Published { new Date(post.createdAt).toLocaleString() } by author#{ post.authorId }</small>
+		<div className="markdown-body" dangerouslySetInnerHTML={getHtml(post.content)} />
 	</article>
 );
 
